a8295b5d160e26fc173e35cbb7239f93de82c0a4
Matt Watson <mwatson@renci.org>
2022-12-21 14:51:14 -0500

    calculate slope at generator

--- src/components/subgroup-table.js
+++ src/components/subgroup-table.js
@@ -1,16 +1,33 @@
+import { useMemo } from 'react'
 import { useApp } from '../context'
-import { InlineMath } from 'react-katex'
+import { BlockMath } from 'react-katex'
 import 'katex/dist/katex.min.css'
 
+const simplify = (numerator, denominator) => {
+  let gcd = (a,b) => b ? gcd(b, a % b) : a
+  gcd = gcd(numerator, denominator)
+  return [numerator / gcd, denominator / gcd]
+}
+
 export const SubgroupTable = () => {
-  const { generator } = useApp()
+  const { generator, params } = useApp()
+
+  const slope = useMemo(() => {
+    if (!generator) {
+      return null
+    }
+    return simplify(3 * generator.x ** 2 + params.a, 2 * generator.y)
+  }, [generator])
 
   return (
     <div>
       {
         generator
-          ? <InlineMath math={ `g = (${ generator.x }, ${ generator.y })` } />
-          : <InlineMath math={ `g = \\text{undefined}` } />
+          ? <BlockMath math={ `g = (${ generator.x }, ${ generator.y })` } />
+          : <BlockMath math={ `g = \\text{undefined}` } />
+      }
+      {
+        slope && <BlockMath math={ `m_g = \\frac{${ slope[0] }}{${ slope[1] }}` } />
       }
     </div>
   )
